4e10d585b025b3637a3dab4b9a8f864e46f0e6da
xiaoxiayan <a690150618@qq.com>
2023-09-08 18:22:15 +0800

    cc

--- behavior.spec.ts
+++ behavior.spec.ts
@@ -38,7 +38,72 @@ it("should create user", () => {
   vi.spyOn(database, "addUser")
   const userService = new UserService(database)
   userService.createUser('aaa')
-  console.log(database.addUser)
-  expect(database.addUser).toBeCalled()
+  userService.createUser('bbb')
+
+  expect(database.addUser).toBeCalledTimes(2)
+
+})
+
+const apiClass = {
+  upload () {
+    return new Promise<number>((resolve, reject) => {
+        resolve(1)
+    })
+  }
+}
+
+class Dialog {
+  private name
+  constructor(private apiClass) {
+
+  }
+  async openDialog  (name, data?, type?) {
+    const r =  await this.apiClass.upload()
+    return r + name
+  }
+}
+
+it("should call updateFn", async () => {
+  const clickBtn = true
+  const API = apiClass
+  const dialogClass = new Dialog(API)
+  vi.spyOn(API, 'upload')
+  console.log(API.upload)
+  const res = await dialogClass.openDialog('xxp')
+  expect(clickBtn).toBe(true)
+  // expect(API.upload).toBeCalled()
+  expect(res).toBe('1xxp')
+
+})
+
+it("should call updateFn", async () => {
+  const clickBtn = true
+  const API = apiClass
+  const dialogClass = new Dialog(API)
+  vi.spyOn(API, 'upload')
+  dialogClass.openDialog
+
+
+})
+
+const getinputDom = (classname) => {
+  const dom = document.getElementsByClassName(classname) as HTMLElement | unknown
+  return '123'
+
+}
+
+it('to ui test', () => {
+  vi.mock('element-puls')
+  const apiData = {
+    name: 1,
+    age: 12,
+    height: 111,
+    like: 'eat'
+  }
+  const uiInput = [apiData.name, apiData.age, apiData.height, apiData.like]
+
+  expect(uiInput[0]).toBe(1)
+
+
 
 })
